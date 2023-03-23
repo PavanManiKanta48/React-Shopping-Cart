b3ced4faf30c8d40d405b39f56a31827bc16521a
PavanManiKanta48 <pavanmanikanta484@gmail.com>
2023-03-23 18:19:49 +0530

    cart

---
+++ src/store.js
@@ -0,0 +1,13 @@
+import { legacy_createStore, applyMiddleware } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
+import thunk from "redux-thunk";
+import reducer from "./reducer";
+
+const middleware = [thunk];
+
+const store = legacy_createStore(
+  reducer,
+  composeWithDevTools(applyMiddleware(...middleware))
+);
+
+export default store;
